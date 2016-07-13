c1012f13e37c88ffe93b0f21b83cb6620724e4a7
fopineda <fopineda95@gmail.com>
2016-07-13 12:08:33 -0400

    homemadeButton-Web
    A couple of buttons I made using the bootstrap grid and a little of CSS
    magic. Enjoy!

---
+++ js/index.js
@@ -0,0 +1,11 @@
+/*
+Created by: Felipe O Pineda
+fopineda95@gmail.com
+Code away :)
+*/
+
+$(document).ready(function(){
+  $(".row .col-xs-10").click(function(){
+    alert("You clicked me :)");
+  });
+});
